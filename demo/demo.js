8d54f0251a913f3629be7ec3e8298b97783dc7cf
KARL JIAHAO LI <31644917+hao94819@users.noreply.github.com>
2022-03-02 23:16:11 -0500

    Add files via upload

--- demo/demo.js
+++ demo/demo.js
@@ -1,28 +1,35 @@
-if (1 === 1) {
-  console.log("yes");
-}
+// if (1 === 1) {
+//   console.log("yes");
+// }
 
-const numbrs = [1, 2, 3, 4, 5];
+// const numbrs = [1, 2, 3, 4, 5];
 
-console.log(
-  numbrs.every((ele) => {
-    return ele > 0;
-  })
-);
-console.log(
-  numbrs.some((ele) => {
-    return ele > 4;
-  })
-);
+// console.log(
+//   numbrs.every((ele) => {
+//     return ele > 0;
+//   })
+// );
+// console.log(
+//   numbrs.some((ele) => {
+//     return ele > 4;
+//   })
+// );
 
-numbrs.forEach((ele) => console.log(ele * 2));
-console.log(
-  numbrs.map((ele) => {
-    return ele * 2;
-  })
-);
+// numbrs.forEach((ele) => console.log(ele * 2));
+// console.log(
+//   numbrs.map((ele) => {
+//     return ele * 2;
+//   })
+// );
 
 function showText() {
   const content = document.getElementById("inputBox2").value;
   console.log(content);
 }
+
+const show = function () {
+  return function () {
+    console.log("Anonymous Functions");
+  };
+};
+show()();
