ac9f9e9908669b5cfefb4bde0ba5d9c54914fcc2
KARL JIAHAO LI <tuf76523@temple.edu>
2022-03-09 11:27:09 -0500

    Add files via upload

--- FullStack_JavaScript/demo/demo.js
+++ FullStack_JavaScript/demo/demo.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // if (1 === 1) {
 //   console.log("yes");
 // }
@@ -33,3 +34,40 @@ const show = function () {
   };
 };
 show()();
+=======
+// if (1 === 1) {
+//   console.log("yes");
+// }
+
+// const numbrs = [1, 2, 3, 4, 5];
+
+// console.log(
+//   numbrs.every((ele) => {
+//     return ele > 0;
+//   })
+// );
+// console.log(
+//   numbrs.some((ele) => {
+//     return ele > 4;
+//   })
+// );
+
+// numbrs.forEach((ele) => console.log(ele * 2));
+// console.log(
+//   numbrs.map((ele) => {
+//     return ele * 2;
+//   })
+// );
+
+function showText() {
+  const content = document.getElementById("inputBox2").value;
+  console.log(content);
+}
+
+const show = function () {
+  return function () {
+    console.log("Anonymous Functions");
+  };
+};
+show()();
+>>>>>>> 7649dbf (test commit from local)
